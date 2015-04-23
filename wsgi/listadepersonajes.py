03c2633c211729a86442221e381dd564c5ac79f0
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-04-23 09:10:29 +0200

    primer paso listar personajes

---
+++ wsgi/listadepersonajes.py
@@ -0,0 +1,11 @@
+from bottle import request
+import json
+import requests
+mi_api='fcf7e44d-5a7c-4699-9f58-88423c69fb9d'
+
+dicc={"champData":"stats","api_key":mi_api}
+lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion",params=dicc)
+if lol.status_code == 200:
+	champions = json.loads(lol.text)
+	for i in champions['data'].keys():
+		print i
