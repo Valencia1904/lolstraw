62ae28a9407dde0f69b75301a802c5d7696e6690
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-04-23 10:33:35 +0200

    preparando objetos.py

--- wsgi/listadepersonajes.py
+++ wsgi/listadepersonajes.py
@@ -6,7 +6,7 @@ mi_api='fcf7e44d-5a7c-4699-9f58-88423c69fb9d'
 dicc={"champData":"stats","api_key":mi_api}
 lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion",params=dicc)
 
-# https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?locale=es_ES&champData=stats&api_key=fcf7e44d-5a7c-4699-9f58-88423c69fb9d EN ESPAÑOL
+# https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?locale=es_ES&champData=stats&api_key=fcf7e44d-5a7c-4699-9f58-88423c69fb9d EN ESPAnOL
 
 if lol.status_code == 200:
 	champions = json.loads(lol.text)
--- wsgi/objetos.py
+++ wsgi/objetos.py
@@ -4,7 +4,9 @@ import requests
 mi_api='fcf7e44d-5a7c-4699-9f58-88423c69fb9d'
 
 # https://global.api.pvp.net/api/lol/static-data/euw/v1.2/item?api_key=fcf7e44d-5a7c-4699-9f58-88423c69fb9d
-# https://global.api.pvp.net/api/lol/static-data/euw/v1.2/item?locale=es_ES&itemListData=stats&api_key=fcf7e44d-5a7c-4699-9f58-88423c69fb9d EN ESPAÑOL
+# https://global.api.pvp.net/api/lol/static-data/euw/v1.2/item?locale=es_ES&itemListData=stats&api_key=fcf7e44d-5a7c-4699-9f58-88423c69fb9d EN ESPAnOL
 
-dicc={"champData":"stats","api_key":mi_api}
-lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion",params=dicc)
+dicc={"locale":"es_ES","itemListData":"stats","api_key":mi_api}
+lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/item",params=dicc)
+if lol.status_code == 200:
+	print lol
