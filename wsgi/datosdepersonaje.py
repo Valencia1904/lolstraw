682a34d6b1bcf964ba87c130ce1cece61bb169e0
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-04-23 09:42:21 +0200

    listar datos de un personaje

---
+++ wsgi/datosdepersonaje.py
@@ -0,0 +1,13 @@
+from bottle import request
+import json
+import requests
+mi_api='fcf7e44d-5a7c-4699-9f58-88423c69fb9d'
+
+dicc={"champData":"stats","api_key":mi_api}
+lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion",params=dicc)
+#nombre=raw_input("Nombre del personaje: ")
+nombre="Fizz"
+if lol.status_code == 200:
+	champions = json.loads(lol.text)
+	for datos in champions['data'][nombre]['stats'].keys():
+		print datos
