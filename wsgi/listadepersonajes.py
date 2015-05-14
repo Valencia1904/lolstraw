287c84daf2e77b91e8853513098d1f8fc5f03b12
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-05-14 10:46:04 +0200

    modificada listapersonajes

--- wsgi/listadepersonajes.py
+++ wsgi/listadepersonajes.py
@@ -7,9 +7,10 @@ dicc={"champData":"stats","api_key":mi_api}
 lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion",params=dicc)
 
 # https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?locale=es_ES&champData=stats&api_key=fcf7e44d-5a7c-4699-9f58-88423c69fb9d EN ESPAnOL
+personajes=[]
 
 if lol.status_code == 200:
 	champions = json.loads(lol.text)
 	for i in champions['data'].keys():
-		print i
-		
+		personajes.append(i)
+print personajes
