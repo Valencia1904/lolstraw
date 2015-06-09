abe4b4623590a5bc92dd64fcfcc1dc74f9732791
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-06-09 17:07:53 +0200

    cominzo de lista 3

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -26,6 +26,7 @@ def listadepersonajes():
 
 #Datos de un personaje
 def datosdepersonaje(nombre):
+	mi_api='fcf7e44d-5a7c-4699-9f58-88423c69fb9d'
 	dicc={"champData":"stats","api_key":mi_api}
 	lol = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion",params=dicc)
 	nombre=name
